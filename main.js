5c4f9d89c82095dbf95577b70e4695f982f91051
Nic Bonetto <nick.bonetto@gmail.com>
2017-06-29 15:56:15 -0700

    Created Car class. #1

---
+++ main.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+
+class Car {
+  constructor(direction, speed, location) {
+    this.direction = direction
+    this.speed = speed
+    this.location = location
+  }
+}
